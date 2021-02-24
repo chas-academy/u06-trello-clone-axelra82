00a645fe2c4aefd6cbb3432e4dcd598d2cb69e96
axelra82 <axelra82@gmail.com>
2021-02-24 15:27:34 +0100

    feat: detect task list id on change

--- js/ui.js
+++ js/ui.js
@@ -1,18 +1,24 @@
-$(".sort-list").sortable({
-	placeholder: "ui-state-highlight",
-	tolerance: "pointer",
-}).disableSelection();
+// $(".sort-list").sortable({
+// 	placeholder: "ui-state-highlight",
+// 	tolerance: "pointer",
+// }).disableSelection();
 
 const makeSortable = () => {
-	$(".sort-task").sortable({
+	$(".sort-task, .sort-list").sortable({
 		connectWith: ".sort-connect",
 		placeholder: "ui-state-highlight",
 		tolerance: "pointer",
-		update: ( event, ui ) => {
-			console.log(event);
-			console.log(ui);
+		start:  (e, ui) => {
+			const startPos = $(e.target).parents('.list').attr('id');
+			ui.item.data('start', startPos);
+		},
+		stop: (e, ui) => {
+			const stopPos = $(e.toElement).parents('.list').attr('id');
+			const startPos = ui.item.data('start');
+			if(startPos !== stopPos){
+				// Change occurred
+			}
 		}
 	}).disableSelection();
-
 	return;
 }
